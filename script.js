bcefb85533c35552da3d632a15a2aa2662f59069
cxml <142420239+cxmlo@users.noreply.github.com>
2023-08-27 17:24:59 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector('.toggle_btn');
-const toggleBtnIcon = document.querySelector('.toggle_btn i');
-const dropDownMenu = document.querySelector('dropdown-menu');
-
-toggleBtn.oneclick = function () {
-    dropDownMenu.classList.toggle('open')
-}
